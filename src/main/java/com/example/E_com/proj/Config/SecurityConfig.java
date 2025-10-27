f6fb7c1e9922290695214c1fd5b7f43242b461b5
Aneeshsshankar <aneeshsshankar@gmail.com>
2025-10-27 13:23:23 +0530

    Added Filter Chain

---
+++ src/main/java/com/example/E_com/proj/Config/SecurityConfig.java
@@ -0,0 +1,37 @@
+package com.example.E_com.proj.Config;
+
+import org.springframework.boot.autoconfigure.security.servlet.UserDetailsServiceAutoConfiguration;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.AuthenticationProvider;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.web.SecurityFilterChain;
+
+@Configuration
+public class SecurityConfig {
+
+    @Bean
+    public SecurityFilterChain securityFilterChain(HttpSecurity http){
+        return http.csrf().disable()
+                .authorizeHttpRequests()
+                .requestMatchers("/api/products").permitAll()
+                .and()
+                .authorizeHttpRequests().requestMatchers("/products").authenticated().and()
+                .formLogin()
+                .and().build();
+//                .sessionManagement()
+//                .sessionCreationPolicy(SessionCreationPolicy.STATELESS)
+//                .and()
+//                .authenticationProvider(authenticationProvider())
+//                .addFilterBefore(auth)
+
+
+
+    }
+
+//    private AuthenticationProvider authenticationProvider() {
+//
+//    }
+
+}
